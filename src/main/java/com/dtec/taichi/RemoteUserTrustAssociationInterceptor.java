3dccf33eff1d4aae1bd2ea28a95da71aa9fdba05
Stephen Matta <stephenjmatta@gmail.com>
2016-09-26 09:40:52 -0400

    Add logging

--- src/main/java/com/dtec/taichi/RemoteUserTrustAssociationInterceptor.java
+++ src/main/java/com/dtec/taichi/RemoteUserTrustAssociationInterceptor.java
@@ -2,16 +2,21 @@ package com.dtec.taichi;
 
 import java.util.Enumeration;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ibm.ObjectQuery.crud.util.ClassName;
 import com.ibm.websphere.security.WebTrustAssociationException;
 import com.ibm.websphere.security.WebTrustAssociationFailedException;
 import com.ibm.wsspi.security.tai.TAIResult;
 import com.ibm.wsspi.security.tai.TrustAssociationInterceptor;
 
-public class RemoteUserTrustAssociationInterceptor implements TrustAssociationInterceptor {  
+public class RemoteUserTrustAssociationInterceptor implements TrustAssociationInterceptor {
+  private static final Logger log = Logger.getLogger(ClassName.class.getName());
+
   private static final String USER_HEADER = "REMOTE_USER";
 
   private String proxy = null;
@@ -33,6 +38,11 @@ public class RemoteUserTrustAssociationInterceptor implements TrustAssociationIn
 
   public boolean isTargetInterceptor(final HttpServletRequest request)
       throws WebTrustAssociationException {
+    log.log(Level.CONFIG,
+        "The remote user trust association interceptor (RUTAI) is configured to accept connections from: {0}",
+        this.proxy);
+    log.log(Level.FINE, "RUTAI sees the remote address: {0}", request.getRemoteAddr());
+    log.log(Level.FINE, "RUTAI sees the remote host: {0}", request.getRemoteHost());
     if (this.proxy != null && (!request.getRemoteAddr().equals(this.proxy)
         && !request.getRemoteHost().equals(this.proxy))) {
       return false;
